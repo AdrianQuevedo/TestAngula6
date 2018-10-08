45e92b6c7c254070261635fc71c299124c162b95
Adrián Quevedo <adrian.quevedo@SDC.AD>
2018-10-08 16:14:05 -0300

    agregado de detalles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Primera pagina Angular 6';
+  texto: String = '';
+
+  constructor() {
+    this.texto = '';
+  }
 }
