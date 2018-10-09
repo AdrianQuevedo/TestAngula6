14bd630e16dfe0a6bb330066e8a2f535d1f1d683
Adrián Quevedo <adrian.quevedo@SDC.AD>
2018-10-09 11:34:36 -0300

    lista de links a descargar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alert } from 'selenium-webdriver';
 
 
 @Component({
@@ -9,8 +10,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Primera pagina Angular 6';
   texto: String = '';
+  links: String [] = [];
+  visible = false;
 
   constructor() {
     this.texto = '';
+  }
+
+  agregarLink(): void {
+    if ( this.texto.trim().toString() !== '') {
+      if ( this.texto.trim().localeCompare('https://www.youtube.com/') === 1 ) {
+        this.links.push(this.texto);
+        this.visible = true;
+    } else {
+      alert('Por favor ingrese un link valido a youtube.' ); }
+    } else {
+      alert('Debe ingresar almenos 1 link para descargar.' ); }
+  }
+
+  eliminarLink(link: String): void {
+    for (let i = 0; i < this.links.length; i++) {
+      console.log('entre 1');
+      if ( this.links[i].trim().toString() === link) {
+        console.log('entre 2');
+        this.links.splice(i, 1);
+        break;
+      }
+    }
   }
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,10 @@
 import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatListModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatListModule, MatToolbarModule, MatIconModule],
 })
 export class MaterialModule { }
